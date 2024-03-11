a6cc659350a20c5a549d91a3db05a39258cd245d
lesteenman <lesteenman@gmail.com>
2024-03-11 20:40:03 +0100

    Add a placeholder page for a bee atlas

--- game/models/bees/bee_species_manager.gd
+++ game/models/bees/bee_species_manager.gd
@@ -36,39 +36,177 @@ static func create_base(species: SPECIES) -> Bee:
 
 enum SPECIES {
 	COMMON,
+	WORKER,
+	SKILLED,
+	CARPENTER,
+	BLACKSMITH,
 	FOREST,
-	WATER
+	LAZY,
+	ENERGETIC,
+	WOODLAND,
+	LAKE,
+	OCEAN,
+	GRASSLANDS,
+	POND,
+	MEADOWS
 }
 
 # All 1-5, for now
 const SPECIES_STATS = {
-	SPECIES.COMMON: {
-		display_name = "common",
-		productivity = 2,
-		fertility = 3,
-		lifespan = 2,
-		color_outline = Color.BLACK,
-		color_1 = Color(1, 0.9, 1, 1),
-		color_2 = Color(0.8, 1, 1, 1),
-	},
-	SPECIES.FOREST: {
-		display_name = "forest",
-		productivity = 3,
-		fertility = 2,
-		lifespan = 3,
-		color_outline = Color.GREEN,
-		color_1 = Color.WEB_GREEN,
-		color_2 = Color.FOREST_GREEN,
-	},
-	SPECIES.WATER: {
-		display_name = "water",
-		productivity = 1,
-		fertility = 3,
-		lifespan = 3,
-		color_outline = Color.BLUE,
-		color_1 = Color.LIGHT_BLUE,
-		color_2 = Color.LIGHT_CYAN,
-	}
+  SPECIES.COMMON: {
+	"display_name": "Common",
+	"parent_1": SPECIES.FOREST,
+	"parent_2": SPECIES.MEADOWS,
+	"color_outline": "#7F7F7F",
+	"color_1": "#228b22",
+	"color_2": "#008000",
+	"fertility": 3,
+	"productivity": 2,
+	"lifespan": 5
+  },
+  SPECIES.WORKER: {
+	"display_name": "Worker",
+	"parent_1": SPECIES.POND,
+	"parent_2": SPECIES.MEADOWS,
+	"color_outline": "#404040",
+	"color_1": "#1e90ff",
+	"color_2": "#ff8c00",
+	"fertility": 2,
+	"productivity": 3,
+	"lifespan": 4
+  },
+  SPECIES.SKILLED: {
+	"display_name": "Skilled",
+	"parent_1": SPECIES.WORKER,
+	"parent_2": SPECIES.FOREST,
+	"color_outline": "#696969",
+	"color_1": "#1e90ff",
+	"color_2": "#228b22",
+	"fertility": 1,
+	"productivity": 4,
+	"lifespan": 3
+  },
+  SPECIES.CARPENTER: {
+	"display_name": "Carpenter",
+	"parent_1": SPECIES.SKILLED,
+	"parent_2": SPECIES.WOODLAND,
+	"color_outline": "#3C3C3C",
+	"color_1": "#d2691e",
+	"color_2": "#228b22",
+	"fertility": 4,
+	"productivity": 5,
+	"lifespan": 2
+  },
+  SPECIES.BLACKSMITH: {
+	"display_name": "Blacksmith",
+	"parent_1": SPECIES.SKILLED,
+	"parent_2": SPECIES.MEADOWS,
+	"color_outline": "#262626",
+	"color_1": "#d2691e",
+	"color_2": "#008000",
+	"fertility": 5,
+	"productivity": 2,
+	"lifespan": 1
+  },
+  SPECIES.FOREST: {
+	"display_name": "Forest",
+	"parent_1": SPECIES.COMMON,
+	"parent_2": SPECIES.POND,
+	"color_outline": "#262626",
+	"color_1": "#228b22",
+	"color_2": "#1e90ff",
+	"fertility": 2,
+	"productivity": 5,
+	"lifespan": 3
+  },
+  SPECIES.LAZY: {
+	"display_name": "Lazy",
+	"parent_1": SPECIES.MEADOWS,
+	"parent_2": SPECIES.POND,
+	"color_outline": "#7F7F7F",
+	"color_1": "#008000",
+	"color_2": "#1e90ff",
+	"fertility": 3,
+	"productivity": 2,
+	"lifespan": 5
+  },
+  SPECIES.ENERGETIC: {
+	"display_name": "Energetic",
+	"parent_1": SPECIES.MEADOWS,
+	"parent_2": SPECIES.FOREST,
+	"color_outline": "#008000",
+	"color_1": "#228b22",
+	"color_2": "#87ceeb",
+	"fertility": 4,
+	"productivity": 4,
+	"lifespan": 2
+  },
+  SPECIES.WOODLAND: {
+	"display_name": "Woodland",
+	"parent_1": SPECIES.FOREST,
+	"parent_2": SPECIES.LAZY,
+	"color_outline": "#3C3C3C",
+	"color_1": "#228b22",
+	"color_2": "#008000",
+	"fertility": 5,
+	"productivity": 3,
+	"lifespan": 3
+  },
+  SPECIES.LAKE: {
+	"display_name": "Lake",
+	"parent_1": SPECIES.POND,
+	"parent_2": SPECIES.LAZY,
+	"color_outline": "#008000",
+	"color_1": "#1e90ff",
+	"color_2": "#87ceeb",
+	"fertility": 4,
+	"productivity": 3,
+	"lifespan": 4
+  },
+  SPECIES.OCEAN: {
+	"display_name": "Ocean",
+	"parent_1": SPECIES.POND,
+	"parent_2": SPECIES.ENERGETIC,
+	"color_outline": "#4D4D4D",
+	"color_1": "#1e90ff",
+	"color_2": "#228b22",
+	"fertility": 5,
+	"productivity": 4,
+	"lifespan": 1
+  },
+  SPECIES.GRASSLANDS: {
+	"display_name": "Grasslands",
+	"parent_1": SPECIES.MEADOWS,
+	"parent_2": SPECIES.FOREST,
+	"color_outline": "#7F7F7F",
+	"color_1": "#228b22",
+	"color_2": "#008000",
+	"fertility": 3,
+	"productivity": 4,
+	"lifespan": 3
+  },
+  SPECIES.POND: {
+	"display_name": "Pond",
+	"parent_1": SPECIES.MEADOWS,
+	"parent_2": SPECIES.LAZY,
+	"color_outline": "#4D4D4D",
+	"color_1": "#87ceeb",
+	"color_2": "#008000",
+	"fertility": 4,
+	"productivity": 5,
+	"lifespan": 2
+  },
+  SPECIES.MEADOWS: {
+	"display_name": "Meadows",
+	"parent_1": SPECIES.FOREST,
+	"parent_2": SPECIES.POND,
+	"color_outline": "#7F7F7F",
+	"color_1": "#32cd32",
+	"color_2": "#8b4513",
+	"fertility": 2,
+	"productivity": 3,
+	"lifespan": 4
+  }
 }
 
 const required_stats = ["display_name", "color_outline", "color_1", "color_2"]
--- game/scenes/main.gd
+++ game/scenes/main.gd
@@ -4,47 +4,75 @@ extends Control
 @onready var bee_inventory = %BeeInventory
 @onready var lab_control: VBoxContainer = %LabControl
 @onready var main_menu: VBoxContainer = %MainMenu
+@onready var bee_atlas_scene: Control = %BeeAtlasScene
+@onready var header: PanelContainer = %Header
 
 var screen_size: Rect2
 var menu_transition_tween
+var navigation_stack: Array
 
 const MENU_TRANSITION_TIME = 0.3
+const HEADER_TRANSITION_TIME = 0.2
 
 func _ready():
 	#var beelab = BeeLabModel.new()
 	bee_lab.beelab = LabManager.labs[0]
 
+	navigation_stack = []
+
 	screen_size = get_viewport_rect()
+
+	lab_control.position = Vector2(screen_size.size.x, 0)
+	bee_atlas_scene.position = Vector2(screen_size.size.x, 0)
 
 	bee_lab.connect("add_item_to_storage", bee_inventory.add_item_to_storage)
 	bee_lab.inventory_handler = bee_inventory
 
-	get_node("MainMenu/Labs").connect("pressed", navigate_to_lab)
+	get_node("MainMenu/Labs").connect("pressed", func(): navigate_forward(lab_control))
+	get_node("MainMenu/SpeciesAtlas").connect("pressed", func(): navigate_forward(bee_atlas_scene))
+
 	lab_control.connect("navigate_back", on_navigate_back)
 
+	set_header_visible(false, false)
+
+func navigate_forward(new_page: Control):
+	var previous_page = navigation_stack[navigation_stack.size() - 1] if navigation_stack.size() else main_menu
+	navigation_stack.append(new_page)
+
+	set_header_visible(navigation_stack.size() > 0, true)
+
+	if menu_transition_tween:
+		menu_transition_tween.kill()
+	menu_transition_tween = create_tween()
+
+	menu_transition_tween.tween_property(
+		new_page, "position", Vector2.ZERO, MENU_TRANSITION_TIME
+	).set_trans(Tween.TRANS_SINE)
+	menu_transition_tween.parallel().tween_property(
+		previous_page, "position", Vector2(-screen_size.size.x, 0), MENU_TRANSITION_TIME
+	).set_trans(Tween.TRANS_SINE)
+
 func on_navigate_back():
-	print("should navigate back", main_menu.get_transform())
+	var previous_page = navigation_stack.pop_back()
+	var current_page = navigation_stack[navigation_stack.size() - 1] if navigation_stack.size() else main_menu
+
+	set_header_visible(navigation_stack.size() > 0, true)
+
+	print("should navigate back", previous_page, current_page)
 
 	if menu_transition_tween:
 		menu_transition_tween.kill()
 	menu_transition_tween = create_tween()
+
 	menu_transition_tween.tween_property(
-		main_menu, "position", Vector2.ZERO, MENU_TRANSITION_TIME
+		current_page, "position", Vector2.ZERO, MENU_TRANSITION_TIME
 	).set_trans(Tween.TRANS_SINE)
 	menu_transition_tween.parallel().tween_property(
-		lab_control, "position", Vector2(screen_size.size.x, 0), MENU_TRANSITION_TIME
+		previous_page, "position", Vector2(screen_size.size.x, 0), MENU_TRANSITION_TIME
 	).set_trans(Tween.TRANS_SINE)
 
-func navigate_to_lab():
-	print("should navigate to lab")
+func set_header_visible(is_visible: bool, animated: bool):
+	var tween = create_tween()
 
-	if menu_transition_tween:
-		menu_transition_tween.kill()
-	menu_transition_tween = create_tween()
-	menu_transition_tween.tween_property(
-		lab_control, "position", Vector2.ZERO, MENU_TRANSITION_TIME
-	).set_trans(Tween.TRANS_SINE)
-	menu_transition_tween.parallel().tween_property(
-		main_menu, "position", Vector2(-screen_size.size.x, 0), MENU_TRANSITION_TIME
-	).set_trans(Tween.TRANS_SINE)
-
+	var new_position = Vector2(0, 0 if is_visible else -header.get_rect().size.y)
+	tween.tween_property(header, "position", new_position, HEADER_TRANSITION_TIME)
